af903e8fe3b7500ecdeae4b6c25b175aff7ab592
jfcode101 <125421939+jfcode101@users.noreply.github.com>
2024-10-17 10:46:15 -0400

    Reset repository to empty state"

--- backend/routes/eventRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createEvent, getEvents } from "../controllers/eventController.js";
-
-const router = express.Router();
-
-router.post("/", createEvent);
-router.get("/", getEvents);
-
-export default router;
--- backend/routes/postRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createPost, getPosts } from "../controllers/postController.js";
-
-const router = express.Router();
-
-router.post("/", createPost);
-router.get("/", getPosts);
-
-export default router;
