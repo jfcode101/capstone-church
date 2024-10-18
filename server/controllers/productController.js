363cfbc629af98878f57897924b46ea2da35ed40
jfcode101 <125421939+jfcode101@users.noreply.github.com>
2024-10-17 20:15:25 -0400

    adds the controller

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,2 +1,76 @@
+import mongoose from "mongoose";
+import Product from "../models/productModel.js";
 
-           
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("error in fetching products:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if (
+    !product.name ||
+    !product.price ||
+    !product.image ||
+    !product.description
+  ) {
+    return res.status(400).json({
+      success: false,
+      message: "Missing data, please check all fields.",
+    });
+  }
+  //  new product
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res.status(201).json({
+      success: true,
+      data: newProduct,
+    });
+  } catch (err) {
+    console.log("Error while creating product", err);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const product = req.body; // user will be able to update the data
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({
+      success: true,
+      data: updatedProduct,
+    });
+  } catch (err) {
+    console.log("Error while updating product", err);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  console.log("id: ", id);
+  try {
+    await Product.findByIdAndDelete(id); // delete the product
+    res.status(200).json({ success: true, message: "Product deleted" }); // send response
+  } catch (err) {
+    console.log("Error while deleting product", err);
+    res.status(404).json({ success: false, message: "Product not found" }); // send response
+  }
+};
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,102 +1,19 @@
 import express from "express";
-import mongoose from "mongoose";
-import Product from "../models/productModel.js";
+import {
+  getProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-// get all products
-router.get("/", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json({
-      success: true,
-      data: products,
-    });
-  } catch (err) {
-    console.log("Error while fetching products", err);
-    res.status(500).json({
-      success: false,
-      message: "Internal Server Error",
-    });
-  }
-});
-
-//  add product to the db
-router.post("/", async (req, res) => {
-  const product = req.body; // user will send the data  in the body
-
-  if (
-    !product.name ||
-    !product.price ||
-    !product.image ||
-    !product.description
-  ) {
-    return res.status(400).json({
-      success: false,
-      message: "Missing data, please check all fields.",
-    });
-  }
-  //  new product
-  const newProduct = new Product(product);
-  try {
-    await newProduct.save();
-    res.status(201).json({
-      success: true,
-      data: newProduct,
-    });
-  } catch (err) {
-    console.log("Error while creating product", err);
-    res.status(500).json({
-      success: false,
-      message: "Internal Server Error",
-    });
-  }
-});
-
-// update product
-router.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const product = req.body; // user will be able to update the data
-  // if (!mongoose.Types.ObjectId.isValid(id)) {
-  //   return res.status(404).json({
-  //     success: false,
-  //     message: "No product with that id",
-  //   });
-  // }
-  try {
-    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
-      new: true,
-    });
-    res.status(200).json({
-      success: true,
-      data: updatedProduct,
-    });
-  } catch (err) {
-    console.log("Error while updating product", err);
-    res.status(500).json({
-      success: false,
-      message: "Internal Server Error",
-    });
-  }
-});
-
-// delete product from db
-router.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  console.log("id: ", id);
-  try {
-    await Product.findByIdAndDelete(id); // delete the product
-    res.status(200).json({ success: true, message: "Product deleted" }); // send response
-  } catch (err) {
-    console.log("Error while deleting product", err);
-    res.status(404).json({ success: false, message: "Product not found" }); // send response
-  }
-});
-
+router.get("/", getProducts); // get all products
+router.post("/", createProduct); // add product to the db
+router.put("/:id", updateProduct); // update product
+router.delete("/:id", deleteProduct); // delete product from db
 router.get("/", (req, res) => {
   res.send("Product Route");
 });
-
-
 
 export default router;
